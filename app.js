edede75f35500189e2e3d8fd3ecfcb5c0949d590
Yash <mehrotra.yash9@gmail.com>
2018-08-28 03:15:58 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express=require('express'),
+	app=express(),
+	port=process.env.port || 3000
+
+app.get('/',function(req,res){
+	res.send("Hello Frands !!");
+})
+
+app.listen(port,function(err){
+	if(err)
+		console.log(err)
+	else
+		console.log("Magic happening at port "+port)
+})
